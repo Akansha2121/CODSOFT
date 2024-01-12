fbd8b0bb464a004fb8d04f9a991369cfbeab6c21
Akansha2121 <154360105+Akansha2121@users.noreply.github.com>
2024-01-12 18:58:20 +0530

    tic-tac-toe.cpp

--- tic-tac-toe.cpp
+++ tic-tac-toe.cpp
@@ -49,7 +49,7 @@ else
 	cout<<"Wrong input!!!";
 	break;
 }
-
+// conditions for winning
 		if((arr[0][1]=='X'&&arr[0][0]=='X'&&arr[0][2]=='X')||(arr[0][0]=='X'&&arr[1][1]=='X'&&arr[2][2]=='X')||(arr[0][2]=='X'&&arr[1][1]=='X'&&arr[2][0]=='X')||(arr[0][2]=='X'&&arr[1][2]=='X'&&arr[2][2]=='X')||(arr[2][0]=='X'&&arr[2][1]=='X'&&arr[2][2]=='X')||(arr[0][0]=='X'&&arr[1][0]=='X'&&arr[2][0]=='X')||(arr[1][0]=='X'&&arr[1][1]=='X'&&arr[1][2]=='X'))
 	{
 			cout<<"\nHURRAY!!\nPlayer 1 wins";
@@ -84,7 +84,7 @@ else
 	break;
 }
 if(i==10)               //starting display is also included so 10
-{ cout<<"\nGAME IS TIED";
+{ cout<<"\nGAME IS TIED";       
 		break;
 	
 
